70958c1ef2da3648732a9604e93153be98c43527
Jacob LaRock <jacob.larock@student.uni-siegen.de>
2025-01-25 14:50:42 +0100

    naming collision change

--- main.py
+++ main.py
@@ -512,7 +512,7 @@ def explainer_test(model):
     #                                                            sample_features,
     #                                                            feature_cols,
     #                                                            1000000)
-    e = explainers.make_explainer(labels, model, features, feature_cols, batch_size=100000, train_data_limit=10000, subset_size=1000, cache_name="e1000convterm")
+    e = explainers.make_explainer(labels, model, features, feature_cols, batch_size=100000, train_data_limit=10000, subset_size=1000, cache_name="e1000convpoolterm")
     for x in range(10000, 30000):
         sample_features = classification.isolate_sample(features, f"{x}.wav")
         out = explainers.explain(model,
@@ -536,9 +536,9 @@ if __name__ == "__main__":
     More specific parameters are in the extraction, train and eval functions, such as dataset directory.
     """
     # experiment metadata 
-    model_name = "ItW_multi_percep_wval_convterm_u10000"
+    model_name = "ItW_multi_percep_wval_convpoolterm_u10000"
     train_cutoff = 10000
 
     train(model_name, train_cutoff)
     evaluate(model_name, train_cutoff)
-    # explainer_test("./trained_models/ItW_multi_percep_wval_convterm_u10000/ItW_multi_percep_wval_convterm_u10000")
+    # explainer_test("./trained_models/ItW_multi_percep_wval_convpoolterm_u10000/ItW_multi_percep_wval_convpoolterm_u10000")
