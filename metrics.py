364ac353888083f74d9234078a75a6bcca3f134e
Jacob LaRock <jacob.larock@student.uni-siegen.de>
2025-02-05 21:09:39 +0100

    add roc curves

--- metrics.py
+++ metrics.py
@@ -1,5 +1,7 @@
 from ast import literal_eval
 from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+import numpy as np
 
 def load_results(path: str) -> list | dict:
     data = open(path, "r").read()
@@ -57,6 +59,43 @@ def eer(data: list) -> dict:
     threshold = res.x
     return {"eer": _error_rate(threshold, data), "threshold": threshold}
 
+def tpr_fpr(data: list, threshold: float) -> tuple[float, float]:
+    summary = {"tp": 0,
+               "tn": 0,
+               "fp": 0,
+               "fn": 0}
+    for res in data:
+        result = 0 if res["result"] < threshold else 1
+        if res["label"] == 0:
+            if result == 0:
+                summary["tn"] += 1
+            if result == 1:
+                summary["fp"] += 1
+        if res["label"] == 1:
+            if result == 0:
+                summary["fn"] += 1
+            if result == 1:
+                summary["tp"] += 1
+    tpr = summary["tp"] / (summary["tp"] + summary["fn"])
+    fpr = summary["fp"] / (summary["fp"] + summary["tn"])
+    return tpr, fpr
+
+def gen_roc(data: list, save_path: str) -> None:
+    x = []
+    y = []
+    for i in range(-10, 110):
+        threshold = i / 100
+        tpr, fpr = tpr_fpr(data, threshold)
+        y.append(tpr)
+        x.append(fpr)
+    plt.clf()
+    plt.rcParams.update({"font.size": 15})
+    plt.ylim(0, max(y))
+    plt.xlim(0, max(x))
+    plt.plot(x, y, linewidth=3)
+    plt.fill_between(x, y, 0, color="b", alpha=0.2)
+    plt.savefig(save_path)
+
 if __name__ == "__main__":
     import sys
     from glob import glob
@@ -67,9 +106,12 @@ if __name__ == "__main__":
     result_list = glob("./trained_models/*/*results.txt")
     for result_path in result_list:
         print()
-        print(result_path.split("/")[-2])
+        model_name = result_path.split("/")[-2]
+        print(model_name)
+        result_dir = result_path[:result_path.rfind("/")+1]
         results = load_results(result_path)
         eer_res = eer(results)
         accuracy_res = accuracy(results, threshold)
         print("eer:", eer_res)
         print("accuracy:", accuracy_res)
+        gen_roc(results, result_dir + "roc.png")
